3fc9fd84cf48009a97905b3fd41df01f897b11f1
vagrant <vagrant@vagrant.vm>
2017-09-01 02:59:45 +0000

    Printing activity atributes for athlete

--- playingwapi.py
+++ playingwapi.py
@@ -1,7 +1,24 @@
 from stravalib.client import Client
 import os 
+from stravalib import unithelper 
 
 
 client = Client()
-authorize_url = client.authorization_url(client_id=os.environ['CLIENT_ID'], 
-                                redirect_uri='http://localhost:8282/authorized')
+# authorize_url = client.authorization_url(client_id=os.environ['CLIENT_ID'], 
+#                                redirect_uri='http://localhost:8282/authorized')
+
+client.access_token = os.environ['ACCESS_TOKEN']
+
+ali = client.get_athlete()
+
+print ali
+
+acts = client.get_activities(limit=10)
+
+for act in acts:
+    print act.id, act.name, unithelper.miles(act.distance), act.description
+
+
+#ryan = client.get_athlete(6777976)
+
+#print ryan.city
